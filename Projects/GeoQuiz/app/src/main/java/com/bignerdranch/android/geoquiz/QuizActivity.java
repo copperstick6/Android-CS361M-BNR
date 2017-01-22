03e5209afbeea0f9dd16f9410bbdcfae915ea0c7
copperstick6 <copperstick6@gmail.com>
2017-01-21 19:54:59 -0600

    finished Chapter 1

--- Projects/GeoQuiz/app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
+++ Projects/GeoQuiz/app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -2,12 +2,19 @@ package com.bignerdranch.android.geoquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class QuizActivity extends AppCompatActivity {
+
+    private Button trueButton;
+    private Button falseButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+        trueButton = (Button) findViewById(R.id.true_button);
+        falseButton = (Button) findViewById(R.id.false_button);
     }
 }
